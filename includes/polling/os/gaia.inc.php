48d360346c6eaa97202d2f5f16b6927197263290
Lukasz Sz <l.szczypin@gmail.com>
2018-06-29 19:10:55 +0200

    second commit. test

--- includes/polling/os/gaia.inc.php
+++ includes/polling/os/gaia.inc.php
@@ -9,6 +9,7 @@
  * @subpackage poller
  * @copyright  (C) 2006-2013 Adam Armstrong, (C) 2013-2016 Observium Limited
  *
+ * Comment for test purpose
  */
 
 if (empty($hardware)) // Fallback since svnApplianceProductName is only supported since R77.10
